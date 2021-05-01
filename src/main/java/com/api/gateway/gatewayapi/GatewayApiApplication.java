bf58ab06d4a5e4fdc96c6ce89eae2b9fdb684f8a
Sofiane Nasri <sofiane.nasri.1995@gmail.com>
2021-05-01 17:30:11 +0200

    change cors policies

--- src/main/java/com/api/gateway/gatewayapi/GatewayApiApplication.java
+++ src/main/java/com/api/gateway/gatewayapi/GatewayApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -21,4 +23,16 @@ public class GatewayApiApplication {
 	public SimpleFilter simpleFilter(){
 		return new SimpleFilter();
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+			
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+
+            }
+        };
+    }
 }
