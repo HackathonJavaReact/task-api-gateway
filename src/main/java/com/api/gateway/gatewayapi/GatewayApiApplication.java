13e1f6e3a6b3a63d29c087439dd6efefa2505cd3
Sofiane Nasri <sofiane.nasri.1995@gmail.com>
2021-05-01 16:59:45 +0200

    Add routing to the authorization api

--- src/main/java/com/api/gateway/gatewayapi/GatewayApiApplication.java
+++ src/main/java/com/api/gateway/gatewayapi/GatewayApiApplication.java
@@ -1,13 +1,24 @@
 package com.api.gateway.gatewayapi;
 
+import com.api.gateway.gatewayapi.filter.pre.SimpleFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableFeignClients
 public class GatewayApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
-
+	
+	@Bean
+	public SimpleFilter simpleFilter(){
+		return new SimpleFilter();
+	}
 }
---
+++ src/main/java/com/api/gateway/gatewayapi/security/ApplicationSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.api.gateway.gatewayapi.security;
+
+import com.api.gateway.gatewayapi.filter.security.AuthenticationFilter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AuthenticationFilter authenticationFilter;
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/authenticationService/*").permitAll()
+                .antMatchers("/**").denyAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+}
